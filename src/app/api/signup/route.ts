80807200b81207cb3204155f334bb5d669567b6e
codebydurvesh <durvesh.gaikwad08@gmail.com>
2026-02-10 20:02:39 +0530

    feat: implement user signup with email verification
    - Added new dependencies for email handling and password hashing.
    - Created VerificationEmail component for sending verification emails.
    - Implemented signup API route to handle user registration and send verification emails.
    - Added sendVerificationEmail helper function to manage email sending logic.
    - Introduced resend service for email delivery.
    - Defined ApiResponse type for consistent API responses.

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,102 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import { success } from "zod";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await request.json();
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken.",
+        },
+        { status: 400 },
+      );
+    }
+
+    const existingUserVerifiedByEmail = await UserModel.findOne({
+      email,
+    });
+
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserVerifiedByEmail) {
+      if (existingUserVerifiedByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "Email is already registered and verified.",
+          },
+          { status: 400 },
+        );
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserVerifiedByEmail.password = hashedPassword;
+        existingUserVerifiedByEmail.verifyCode = verifyCode;
+        existingUserVerifiedByEmail.verifyCodeExpiry = new Date(
+          Date.now() + 3600000,
+        );
+        await existingUserVerifiedByEmail.save();
+      }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1); // 1 hour expiry
+
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+      await newUser.save();
+    }
+
+    //send verification email
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode,
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 },
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message:
+          "User registered successfully. Verification email sent, Please verify your email.",
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error during signup:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Internal server error during signup.",
+      },
+      { status: 500 },
+    );
+  }
+}
