51ce99ba8cbebfcf46703c977b4d69699c059243
codebydurvesh <durvesh.gaikwad08@gmail.com>
2026-03-02 07:42:17 +0530

    add home page

---
+++ src/data/messages.ts
@@ -0,0 +1,258 @@
+const messages = [
+  {
+    title: "Message from User101",
+    content: "Hey, how’s your day going?",
+    received: "5 minutes ago",
+  },
+  {
+    title: "Message from AnonymousFan",
+    content: "Loved your latest upload!",
+    received: "15 minutes ago",
+  },
+  {
+    title: "Message from CuriousMind",
+    content: "What inspired you to start coding?",
+    received: "30 minutes ago",
+  },
+  {
+    title: "Message from User202",
+    content: "Can you share your tech stack?",
+    received: "45 minutes ago",
+  },
+  {
+    title: "Message from SecretAdmirer",
+    content: "You’re doing amazing work!",
+    received: "1 hour ago",
+  },
+  {
+    title: "Message from DevGuy",
+    content: "Any tips for learning React?",
+    received: "1 hour ago",
+  },
+  {
+    title: "Message from NightOwl",
+    content: "Are you working on something new?",
+    received: "2 hours ago",
+  },
+  {
+    title: "Message from MysteryGuest",
+    content: "Book recommendations please?",
+    received: "3 hours ago",
+  },
+  {
+    title: "Message from User303",
+    content: "How do you stay productive?",
+    received: "4 hours ago",
+  },
+  {
+    title: "Message from SilentReader",
+    content: "Your posts are very helpful!",
+    received: "5 hours ago",
+  },
+
+  {
+    title: "Message from TechFan",
+    content: "Which laptop do you use?",
+    received: "6 hours ago",
+  },
+  {
+    title: "Message from CodeLearner",
+    content: "How did you learn JavaScript?",
+    received: "7 hours ago",
+  },
+  {
+    title: "Message from User404",
+    content: "Any advice for beginners?",
+    received: "8 hours ago",
+  },
+  {
+    title: "Message from Anonymous",
+    content: "Keep pushing forward!",
+    received: "9 hours ago",
+  },
+  {
+    title: "Message from GrowthSeeker",
+    content: "How do you manage time?",
+    received: "10 hours ago",
+  },
+  {
+    title: "Message from User505",
+    content: "What’s your favorite framework?",
+    received: "11 hours ago",
+  },
+  {
+    title: "Message from DesignFan",
+    content: "Your UI looks clean!",
+    received: "12 hours ago",
+  },
+  {
+    title: "Message from Motivator",
+    content: "Don’t stop creating!",
+    received: "13 hours ago",
+  },
+  {
+    title: "Message from EarlyBird",
+    content: "Do you code in the morning?",
+    received: "14 hours ago",
+  },
+  {
+    title: "Message from User606",
+    content: "Any internship tips?",
+    received: "15 hours ago",
+  },
+
+  {
+    title: "Message from AIEnthusiast",
+    content: "Are you exploring AI?",
+    received: "16 hours ago",
+  },
+  {
+    title: "Message from WebWizard",
+    content: "How do you deploy projects?",
+    received: "17 hours ago",
+  },
+  {
+    title: "Message from User707",
+    content: "What’s your next goal?",
+    received: "18 hours ago",
+  },
+  {
+    title: "Message from CuriousCat",
+    content: "How long do you code daily?",
+    received: "19 hours ago",
+  },
+  {
+    title: "Message from FrontendFan",
+    content: "React or Next.js?",
+    received: "20 hours ago",
+  },
+  {
+    title: "Message from BackendBro",
+    content: "Do you use Node.js?",
+    received: "21 hours ago",
+  },
+  {
+    title: "Message from User808",
+    content: "Database of choice?",
+    received: "22 hours ago",
+  },
+  {
+    title: "Message from DevDreamer",
+    content: "What motivates you?",
+    received: "23 hours ago",
+  },
+  {
+    title: "Message from NightCoder",
+    content: "Coffee or tea while coding?",
+    received: "1 day ago",
+  },
+  {
+    title: "Message from User909",
+    content: "Any side projects?",
+    received: "1 day ago",
+  },
+
+  {
+    title: "Message from StartupFan",
+    content: "Would you start a startup?",
+    received: "1 day ago",
+  },
+  {
+    title: "Message from MentorMind",
+    content: "Who inspires you?",
+    received: "1 day ago",
+  },
+  {
+    title: "Message from User111",
+    content: "Favorite coding playlist?",
+    received: "1 day ago",
+  },
+  {
+    title: "Message from HiddenFan",
+    content: "Your growth is inspiring!",
+    received: "2 days ago",
+  },
+  {
+    title: "Message from QuickLearner",
+    content: "Best way to learn DSA?",
+    received: "2 days ago",
+  },
+  {
+    title: "Message from User222",
+    content: "Any hackathon plans?",
+    received: "2 days ago",
+  },
+  {
+    title: "Message from SilentSupporter",
+    content: "Keep going strong!",
+    received: "2 days ago",
+  },
+  {
+    title: "Message from CloudExplorer",
+    content: "Are you learning cloud?",
+    received: "2 days ago",
+  },
+  {
+    title: "Message from User333",
+    content: "How do you debug errors?",
+    received: "3 days ago",
+  },
+  {
+    title: "Message from CodeMaster",
+    content: "Tabs or spaces?",
+    received: "3 days ago",
+  },
+
+  {
+    title: "Message from PixelPerfect",
+    content: "How do you design layouts?",
+    received: "3 days ago",
+  },
+  {
+    title: "Message from User444",
+    content: "What’s your dream company?",
+    received: "3 days ago",
+  },
+  {
+    title: "Message from LearningDev",
+    content: "Any roadmap for full stack?",
+    received: "4 days ago",
+  },
+  {
+    title: "Message from AmbitiousSoul",
+    content: "Where do you see yourself in 5 years?",
+    received: "4 days ago",
+  },
+  {
+    title: "Message from User555",
+    content: "Favorite VS Code extensions?",
+    received: "4 days ago",
+  },
+  {
+    title: "Message from DeepThinker",
+    content: "What’s the hardest bug you fixed?",
+    received: "5 days ago",
+  },
+  {
+    title: "Message from User666",
+    content: "Do you contribute to open source?",
+    received: "5 days ago",
+  },
+  {
+    title: "Message from FriendlyNeighbor",
+    content: "Keep sharing knowledge!",
+    received: "6 days ago",
+  },
+  {
+    title: "Message from User777",
+    content: "Any productivity hacks?",
+    received: "6 days ago",
+  },
+  {
+    title: "Message from FinalFan",
+    content: "Excited for your next project!",
+    received: "1 week ago",
+  },
+];
+
+export default messages;
